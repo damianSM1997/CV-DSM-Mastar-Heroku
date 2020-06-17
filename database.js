fc55f21d4ddb56c0defc027f54b7b3b4bce2b071
damianSM1997 <damiansm97@gmail.com>
2020-06-17 03:31:29 -0500

    variablrs de entorno- comentrios-listo

--- database.js
+++ database.js
@@ -6,14 +6,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
 let urlDB;
-let urlDB1;
+
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost/notes-app'
 } else {
+    //esta sintaxis es la correcta de lo contrario heroku no reconocera
+    // la variable de entorno, bueno en este caso
     urlDB = process.env.MONGO_URI;
-    //'mongodb+srv://damian:MI0E1zcZXQP20IwK@cluster0-naamb.mongodb.net/notasCV?retryWrites=true&w=majority'
-    //urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
 
@@ -24,20 +24,3 @@ mongoose.connect(process.env.URLDB, {
         useNewUrlParser: true
     }).then(db => console.log('la base de datos esta conectada'))
     .catch(err => console.log(err));
-
-
-
-
-//esto ya no sirve xd
-
-
-//const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
-
-//const MONGODB_URI = 'mongodb://localhost/notes-app'
-
-//const MONGODB_URI = 'mongodb+srv://damian:MI0E1zcZXQP20IwK@cluster0-naamb.mongodb.net/notasCV?retryWrites=true&w=majority'
-//const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
-
-//const NOTES_APP_MONGODB_HOST = process.env.NOTES_APP_MONGODB_HOST;
-
-//const NOTES_APP_MONGODB_DATABAse = process.env.NOTES_APP_MONGODB_DATABASE;
