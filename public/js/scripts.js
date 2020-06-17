19f154c80ba8226323e38a370d049d3c60825226
damianSM1997 <damiansm97@gmail.com>
2020-06-17 04:30:58 -0500

    modificaciones en el about

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,10 +3,3 @@ $('#btn-toggle-comment').click(e => {
     e.preventDefault();
     $('#post-comment').slideToggle();
 });
-
-const boton = document.querySelector('#btn-toggle-comment').parentElement.parentElement.parentElement;
-const formulario = document.querySelector('#formulario');
-
-console.log(formulario);
-
-console.log(boton);
